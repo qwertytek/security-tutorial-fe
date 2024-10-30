39397f24b83d8ad72b8acea49aa9a5fea50475b8
Rui Vilela <tek@qwertytek.com>
2024-10-30 23:59:33 +0000

    added connection to fastapi backend

--- src/components/forms/login.tsx
+++ src/components/forms/login.tsx
@@ -7,10 +7,14 @@ export default function LoginForm() {
     async function submit(e: FormEvent<HTMLFormElement>) {
         e.preventDefault();
         const formData = new FormData(e.target as HTMLFormElement);
+
         const response = await fetch("/api/login", {
             method: "POST",
             body: formData,
         });
+
+        console.log('response', response)
+
         const data = await response.json();
         if (data.message) {
             setResponseMessage(data.message);
@@ -25,7 +29,7 @@ export default function LoginForm() {
             </label>
             <label>
                 Email
-                <input type="email" id="email" name="email" required />
+                <input type="password" id="password" name="password" required />
             </label>
             <button>Send</button>
             {responseMessage && <p>{responseMessage}</p>}
--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -2,9 +2,9 @@ import type { APIRoute } from "astro";
 
 export const POST: APIRoute = async ({ request }) => {
     const data = await request.formData();
+
     const username = data.get("username");
-    const password = data.get("email");
-    console.log("data", data, password, username);
+    const password = data.get("password");
 
     if (!username || !password) {
         return new Response(
@@ -14,11 +14,24 @@ export const POST: APIRoute = async ({ request }) => {
             { status: 400 },
         );
     }
+    const response = await fetch(
+        `${import.meta.env.BACKEND_API_BASE_URL}/token`,
+        {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded'},
+            body: new URLSearchParams({
+                username: String(username),
+                password: String(password)
+            }),
+        }
+    )
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Something went wrong');
+        }
+        return response.json();
+    })
 
-    return new Response(
-        JSON.stringify({
-            message: "Success!",
-        }),
-        { status: 200 },
-    );
+
+    return new Response(response);
 };
